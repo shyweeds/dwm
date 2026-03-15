7fdac1afe2b6caecde0f630d335d5cb9d8adc05a
shyweeds <2149934895@qq.com>
2026-03-15 18:44:33 +0800

    config_brightnessctl

--- config.h
+++ config.h
@@ -1,4 +1,8 @@
 /* See LICENSE file for copyright and license details. */
+/* (my)Add system key libs */
+#include <X11/XF86keysym.h>
+#define BrightUp    XF86XK_MonBrightnessUp        /* Monitor/panel brightness */
+#define BrightDown  XF86XK_MonBrightnessDown      /* Monitor/panel brightness */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -58,11 +62,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "alacritty", NULL };
+static const char *brightup[]   = { "brightnessctl", "set", "+5%", NULL };
+static const char *brightdown[] = { "brightnessctl", "set", "5%-", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ 0,                            BrightUp,  spawn,          {.v = brightup } },
+	{ 0,                            BrightDown,spawn,          {.v = brightdown } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
