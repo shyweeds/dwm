cdd3cbae6f96f54f6e62344130902041e6bed7df
shyweeds <2149934895@qq.com>
2026-03-19 18:19:35 +0800

    change_border_pixel

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 #define BrightDown  XF86XK_MonBrightnessDown      /* Monitor/panel brightness */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
